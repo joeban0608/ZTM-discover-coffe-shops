a405020db17ef282d6445e9b67d42e1af5290c87
joebanhong <joeanddaneil5@gmail.com>
2023-03-20 00:30:35 +0800

    - add Link component

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -1,13 +1,20 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
 
 const coffeeStore = () => {
-  const router = useRouter()
-  const {query} = router
-  console.log('router', router)
+  const router = useRouter();
+  const { query } = router;
+  console.log("router", router);
   return (
-    <div>coffee store page {query.id}</div>
-  )
-}
+    <div>
+      coffee store page {query.id}
+      {/* scroll true, link to page and up to top */}
+      <Link href="/" legacyBehavior scroll={true}>
+        <a> Back to home</a>
+      </Link>
+    </div>
+  );
+};
 
-export default coffeeStore
+export default coffeeStore;
